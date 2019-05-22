81c78595423c03e29f1654bb85dfe80aae6056d9
Temwa Manda <tmanda@jhpiego.org>
2019-05-22 17:06:04 +0200

    added index.php

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<link rel="stylesheet" href="style2.css"/>
+<title>CSAT</title>
+</head>
+<body>
+<div class="sidenav">
+  <a href="#Home">HOME</a>
+  <a href="#services">ABOUT US</a>
+  <a href="#clients">CONTACT </a>
+  <a href="#contact">LOCATION</a>
+  <a href="#vision">VISION</a>
+  <a href="#MISSION">MISSION</a>
+</div>
+
+<header>
+<div class="image" > 
+    <img src="big.png"> 
+</div>
+</header>
+<div class="main">
+
+  <h3 class="header">Centre for Accountability and Transparency </h3>
+
+  <p>Strengthening and Improving Service Delivery.</p>
+
+ <b> WHO WE ARE AND WHAT WE DO</b>
+<p>
+    We are a non-profit evidence based accountability organization registered in Malawi under the companies Act (Cap 46:03) of the Laws of Malawi, a member of Council for Non-Governmental of Malawi (CONGOMA), and licensed by NGO Board of Malawi to operate as a Non-Governmental Organization under the NGO Act (Cap 5:05) 2018 of the Laws of Malawi.  We are born as a developmental organization that promotes healthy interaction and constructive engagement between citizens, service providers and policy makers to enhance mutual accountability and transparency in the use of public resources. We focus on issues that worry and concern citizen lives through consultations with various stakeholders, capacity building of communities to hold government accountable, monitor policy priorities and implementation of budgetary processes. We aim to undertake, together with citizens accountability advocate and social audit in local development programs in sectors such economic, social, health, agriculture, infrastructure development, education and mining. We therefore seek to harness information, and citizen participation to strengthen accountability from public officials. We shall always implement creative and evidence based projects that promote citizen voices to reclaim their rights, and hold pubic officers accountable for improved service delivery, greater budget transparency, open contracting and regulation.
+</p>
+
+
+<b>VISION</b>
+<P>
+    We envision an empowered society where duty bearers are accountable and responsive to the citizenry’ demands through constructive engagement.
+</P>
+
+<b>MISSION</b>
+<P>
+To promote constructive and mutual engagement between duty bearers and the citizens to improve service delivery, greater budget transparency, and open contracting by strengthening the voice, capacities and opportunities of citizens to participate in planning, and budget processes, involvement in the governance, monitoring and oversight of service delivery that lead to poverty reduction, and improvement of lives.
+</P>
+
+<b>OUR THEORY OF CHANGE</b>
+<P>
+We believe that social accountability is not a one-time thing; duty bearers should be held accountable at all levels of development. We strongly trust that social accountability is not about exposing the vulnerabilities of public institutions, or punishing public officials. Our theory of change is premised on constructive engagement; an approach that builds on a reciprocal relationship between citizens and public institutions based on mutual respect, trust, legitimacy, transparency and competency.  We are therefore convinced that the four key principles of transparency, accountability, participation and inclusion are the cornerstones for any significant sustainable development in any country across the global especially in developing countries.
+We believe therefore, that by responding to these four key principles, we are assured of achieving good democratic governance, reducing corruption, and mismanagement of public resources social justice and sustainable development, which leads to job creation, income generation, and above all improving the living standards of people and poverty reduction. Therefore, donors, state and non-state actors must incorporate the four key principles in their social contract in order for government to be accountable, and responsive to the demands of citizens.
+</P>
+
+<b>OUR DEFINITE CHIEF AIM</b>
+<P>
+We seek to work in collaboration with like-minded people and institutions to eradicate all forms suffering, indignity and desolateness resulting from high levels of corruption, abuse of public resources, exclusion and low citizen participation. We aim to strengthen capacities of citizens to demand better quality public services and hold government, policy makers and service providers accountable.
+</P>
+
+<b>OUR FUNDAMENTAL COMMITMENT</b>
+
+<p>
+Despite various respectable stakeholders’ efforts, we are perturbed by the increasing levels of mismanagement of public resources and poor service delivery causing vicious levels of poverty and sufferings. These conditions continue to lead into permanent loss of human dignity and in worse case scenarios; needless loss of lives. We therefore commit ourselves to fill the gaps left by the interventions of other institutions. In partnership with stakeholders; we undertake to work with citizens, government and service providers to create a transparent and responsive society.
+GEOGRAPHICAL IMPACT AREAS
+We shall intervene and implement projects based on sectorial assessment in each of the geographical districts in Malawi at any given time depending on availability of funding. Our interventions shall not be guided by funding opportunities; rather we shall seek funding based on programme needs. In whichever case, we shall work in close collaboration with district council leadership of each of the districts in which we shall intervene. We shall be an active stakeholder and development partner of the district councils. We shall not restrict our interventions to any particular district, rather projects will be implemented in all areas where needs exist.
+</p>
+
+
+<b>THEMATIC AREAS</b>
+CSAT work focuses on four broad thematic areas, namely;
+<p>
+<b>1.	CIVIC ENGAGEMENT </b>
+Citizens play a critical role in advocating and helping to make public institutions more transparent, accountable and effective, and contributing innovative solutions to complex development challenges. We believe that sustainable development requires active and informed citizens who understand how to voice their interests, act collectively and hold public officials accountable. Our programs are tailored to help citizens engage actively in the development process and serve as a link between citizens and public officials. In this way, CSAT helps give a voice to citizens who traditionally have no access to government decision-making.
+</p>
+<p>
+<b>2.	CAPACITY BUILDING</b>
+Building effective, transparent, accountable and inclusive public institutions involves enhancing knowledge and capacities of relevant stakeholders; citizens, duty bearers including elected officials (parliamentarians and ward councilors). CSAT programs support efforts to equip citizens with right knowledge to monitor public services, track public expenditures, and participate in the local and national budget processes.
+</p>
+
+<p>
+<b>3.	PUBLIC FINANCE MANAGEMENT SYSTEMS</b>
+We have programs designed to promote greater citizen participation and oversight around government revenues and expenditures through public revenue monitoring, social auditing, participatory budget monitoring and open contracting to contribute to fiscal transparency. 
+</p>
+
+<p>
+<b>4.	RESEARCH, ADVOCACY AND POLICY CHANGE</b>
+Our programming revolves around research, advocacy and pushing for policy change in consultation with relevant stakeholders to respond to citizens’ challenges, modalities and provide platforms for civic engagement.
+</p>
+
+<b>STRATEGIC OBJECTIVES</b>
+
+
+<b>CORE VALUES </b>
+<p>
+<b>INTEGRITY</b>
+We believe in performing our duties in honesty and trustfulness
+</p>
+
+<p>
+<b>PATRIOTIC</b>
+We believe in supporting and helping our communities to do better in implementing local development by stay informed about development projects taking place in their communities
+</p>
+
+<p>
+<b>COMMUNITY ENGAGEMENT</b>
+We believe in community groups working together with service providers and policy makers to address issues that impact the well-being of those groups so that they should be committed to their local development goals and vision 
+</p>
+
+<p>
+<b>TEAM WORK</b>
+We believe in the process of working collaboratively with individuals in planning, problem solving and decision making to better serve the communities – beneficiaries 
+(that have different skills and providing constructive feedback regardless any person differences between individuals)
+</p>
+
+<p>
+<b>EVIDENCE BASED</b>
+We believe in promoting Evidence-Based Practice by having the well-researched interventions with experience and ethics, client preferences and culture to guide and inform the delivery of treatment and services
+</p>
+
+<p>
+AREA OF FOCUS
+Capacity building
+Awareness 
+Community monitoring and assessment
+</p>
+
+<p>
+APPROACHES 
+Public hearing
+Score cards
+Report cards
+Social audit
+</p>
+</div> 
+
+
+</body>
+</html> 
